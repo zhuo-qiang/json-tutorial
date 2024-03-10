8a8d3453426ddc5f099e6fe659c8e678eb8c70f6
zhuo-qiang <1792918514@qq.com>
2024-03-10 12:36:06 +0800

    finish tutorial

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -6,10 +6,16 @@
 
 typedef struct {
     const char* json;
+    /*
+        json is a point to char const
+        说明这个是指针可变, 但是指针指着的json数据不可变
+        不可以通过这个指针进行修改
+        但是指针的指向是可以改变的
+    */
 }lept_context;
 
 static void lept_parse_whitespace(lept_context* c) {
-    const char *p = c->json;
+    const char* p = c->json;
     while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
         p++;
     c->json = p;
@@ -24,21 +30,47 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+static int lept_parse_true(lept_context* c, lept_value* v) {
+    EXPECT(c, 't');
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    v->type = LEPT_TRUE;
+    return LEPT_PARSE_OK;
+}
+static int lept_parse_false(lept_context* c, lept_value* v) {
+    EXPECT(c, 'f');
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    v->type = LEPT_FALSE;
+    return LEPT_PARSE_OK;
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
-        case 'n':  return lept_parse_null(c, v);
-        case '\0': return LEPT_PARSE_EXPECT_VALUE;
-        default:   return LEPT_PARSE_INVALID_VALUE;
+    case 'n':  return lept_parse_null(c, v);
+    case '\0': return LEPT_PARSE_EXPECT_VALUE;
+    case 't': return lept_parse_true(c, v);
+    case 'f': return lept_parse_false(c, v);
+    default:   return LEPT_PARSE_INVALID_VALUE;
     }
 }
 
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
+    int ret;
     assert(v != NULL);
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    return lept_parse_value(&c, v);
+    if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+        lept_parse_whitespace(&c);
+        if (*c.json != '\0') {
+            ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+        }
+    }
+    return ret;
 }
 
 lept_type lept_get_type(const lept_value* v) {
--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -1,7 +1,22 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
+/*
+    include guards防止多次定义头文件
+    习惯是 _H__ 作为后缀
+    大型工程可以用 项目名称_目录_文件名称_H__
+
+    现在可以使用 #pragma once 代替, 这是一个非标准但是主流都支持的指令
+*/
+
 
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+/*
+    json支持的所有数据类型, 声明一个枚举类型
+    枚举类型通常全大写, 而且一般用项目简称为前缀 LEOT_XXX
+
+    c++ 可以用命名空间功能代替
+*/
+
 
 typedef struct {
     lept_type type;
@@ -10,11 +25,24 @@ typedef struct {
 enum {
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
+    /*若一个 JSON 只含有空白*/
     LEPT_PARSE_INVALID_VALUE,
+    /*若一个值之后，在空白之后还有其他字符*/
     LEPT_PARSE_ROOT_NOT_SINGULAR
+    /*若值不是那三种字面值*/
 };
 
 int lept_parse(lept_value* v, const char* json);
+/*
+    解析 JSON ,
+    传入的 json 是 char* 格式, 由于内部不应该对json进行修改, 所以要 const 修饰
+    lept_value* 是传出参数
+    一般用法是
+    lept_value v;
+    const char json[] = ...;
+    int ret = lept_parse(&v, json);
+    返回值 int 对应的是上面的枚举类型
+*/
 
 lept_type lept_get_type(const lept_value* v);
 
