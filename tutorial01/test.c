ae798a4d68d3ca7c3dab047d7afb8baddcdb90f5
zhuo-qiang <1792918514@qq.com>
2024-03-10 13:08:08 +0800

    test 冲突

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -82,8 +82,8 @@ static void test_parse() {
 int main() {
     test_parse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
-    /*
     return main_ret;
+    /*
         测试 git
     */
 }
